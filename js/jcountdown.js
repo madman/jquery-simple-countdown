e2d34779a35549cf40eaf8e8e0455725e61dbf0a
Yuriy Prokopets <yuriy.prokopets@gmail.com>
2013-10-23 12:29:23 +0300

    fixed copyright

---
+++ js/jcountdown.js
@@ -0,0 +1,74 @@
+/*!
+ * jQuery Simple Countdown Plugin v1.0
+ * https://github.com/madman/jquery-simple-countdown
+ *
+ * Copyright 2013 Yuriy Prokopets 
+ * Released under the MIT license
+ */
+(function($) {
+	$.fn.countdown = function(options, callback){
+
+		if ($.isFunction(options)) {
+			callback = options;
+			options = {};
+		}
+
+		var defaults = {
+ 			data: 'time',
+ 			flag: 'countdown',
+ 			callback: 'callback',
+		};
+		var settings = $.extend({}, defaults, options);
+
+
+		var intToTime = function(val) {
+			var 
+				hours = (Math.floor(val / 3600)),
+				minutes = (Math.floor(val / 60) - (Math.floor(val / 3600) * 60)),
+				seconds = val % 60;
+			
+			if (hours < 10) {
+				hours = '0' + hours;
+			}
+		
+			if (minutes < 10) {
+				minutes = '0' + minutes;
+			}
+		
+			if (seconds < 10) {
+				seconds = '0' + seconds;
+			}
+			
+			return hours + ":" + minutes + ":" + seconds;
+		}
+		
+		var step = function(timer) {
+			var 
+				callback,
+				left = parseInt(timer.data(settings.data));
+			
+			timer.text(intToTime(left));
+			
+			if (left > 0) {
+				timer.data(settings.data, left - 1);
+				setTimeout(function() {step(timer)},1000);
+			} else if (callback = timer.data(settings.callback)) {
+				if ($.isFunction(callback)) {
+					callback.call(null, timer);
+				}
+			}
+		}
+	
+		return this.each(function() {
+			var self = $(this);
+
+	   		if (!self.data(settings.flag)) {
+				self.data(settings.flag, true);
+				self.data(settings.callback, callback);
+				
+				step(self);
+			}
+	
+	    });
+	};
+}(jQuery));
